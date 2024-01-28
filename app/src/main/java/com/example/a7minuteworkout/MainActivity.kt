b86b223fc4dd017e94c3d011a2815e736245d147
its-aky <kumaramityadav230@gmail.com>
2024-01-28 17:14:32 +0530

    Using ViewBinding instead of findViewbyID

--- app/src/main/java/com/example/a7minuteworkout/MainActivity.kt
+++ app/src/main/java/com/example/a7minuteworkout/MainActivity.kt
@@ -4,17 +4,34 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.FrameLayout
 import android.widget.Toast
+import com.example.a7minuteworkout.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    //  The binding is name just like the name of the layout with Binding attached
+    //Todo 1:We create a variable for it and assign to null
+    private var binding: ActivityMainBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //Todo 2: We inflate the late file by calling inflate on the Binding name
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        //Todo 3: Then replace the setContentView parameter with binding?.root
+        setContentView(binding?.root)
 
-        val flstartbutton:FrameLayout=findViewById(R.id.fl_start)
-        flstartbutton.setOnClickListener{
-            Toast.makeText(this,"STOP",
-                Toast.LENGTH_LONG
-                ).show()
+        binding?.flStart?.setOnClickListener {
+            Toast.makeText(
+                this@MainActivity,
+                "Here we will start the exercise.",
+                Toast.LENGTH_SHORT
+            ).show()
         }
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        //Todo 4: TO avoid memory leak we unassign the binding once the activity is destroyed
+        binding = null
     }
 }
+
