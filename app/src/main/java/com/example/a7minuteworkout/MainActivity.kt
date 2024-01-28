a55232380801b80ffb54960b72ebac5c2bd20150
its-aky <kumaramityadav230@gmail.com>
2024-01-28 16:58:22 +0530

    Setting Up the UI

--- app/src/main/java/com/example/a7minuteworkout/MainActivity.kt
+++ app/src/main/java/com/example/a7minuteworkout/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.a7minuteworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val flstartbutton:FrameLayout=findViewById(R.id.fl_start)
+        flstartbutton.setOnClickListener{
+            Toast.makeText(this,"STOP",
+                Toast.LENGTH_LONG
+                ).show()
+        }
     }
 }
